d73dcd3c10f5e03e03e382610a6361bbbfc4bfd7
Adam Christopher B Widjaja <adam.keristoper@gmail.com>
2017-11-17 13:38:55 +0900

    click event functional, prop types for todo

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -3,21 +3,49 @@ import buttonPic from '../images/button.svg';
 import upBtn from '../images/up_arrow.svg';
 import buttonGreen from '../images/button_green.svg';
 import buttonRemove from '../images/button_close.svg';
+import PropTypes from 'prop-types';
+
+class TodoListItem extends Component {
+  constructor(props) {
+    super(props);
+    this._onClick = this._onClick.bind(this);
+  }
+
+  _onClick(e) {
+    this.props.toggleTodo(this.props.index)
+  }
+
+  render() {
+    let todoState = this.props.todo.active;
+    let buttonState = (todoState ? buttonGreen : buttonPic);
+    let todoStateClass = (todoState ? "active" : "");
+    return(
+      <li className="todo-item">
+        <span className="todo-check">
+          <img
+            src={buttonState}
+            alt="todo button"
+            className="todo-button-img"
+            onClick={this._onClick} />
+        </span>
+        <span className={"todo-text "+todoStateClass}>{this.props.todo.value}</span>
+        <span className="todo-remove-span">
+          <img src={buttonRemove} alt="todo delete" className="todo-remove-img" />
+        </span>
+      </li>
+    )
+  }
+}
 
 const TodoList = (props) => (
   <ul className="todo-ul">
     { props.todos.map((todo, index) => {
       return(
-        <li key={index}
-          className="todo-item">
-          <span className="todo-check">
-            <img src={buttonGreen} alt="todo button" className="todo-button-img" />
-          </span>
-          <span className="todo-text">{todo}</span>
-          <span className="todo-remove-span">
-            <img src={buttonRemove} alt="todo delete" className="todo-remove-img" />
-          </span>
-        </li>
+        <TodoListItem
+          key={index}
+          index={index}
+          todo={todo}
+          toggleTodo={props.toggleTodo} />
       )
     })}
   </ul>
@@ -43,22 +71,47 @@ class Todo extends Component {
     super(props);
     this.state = {
       todos: [
-        "eat bananas", "bake potatoes", "fry bacon",
-        "eat bananas", "bake potatoes", "fry bacon",
-        "バナナを食べる", "ポテトをフライする", "選択？分からない",
+        {"active": false, "value": "eat bananas"},
+        {"active": false, "value": "bake potatoes"},
+        {"active": true, "value": "fry bacon"},
       ],
       todoinput: "",
     }
+    this.toggleTodo = this.toggleTodo.bind(this)
+  }
+
+  toggleTodo(clicked) {
+    let newArray = this.state.todos.map((todo, index) => {
+      if (clicked === index) {
+        todo.active = !this.state.todos[index].active
+      }
+      return todo;
+    });
+    this.setState({
+      todos: newArray,
+    });
   }
 
   render() {
     return(
       <div className="todo-wrap">
-        <TodoList todos={this.state.todos}/>
+        <div className="torn-paper"></div>
+        <TodoList todos={this.state.todos} toggleTodo={this.toggleTodo}/>
         <TodoInput />
       </div>
     )
   }
+}
+
+TodoList.propTypes = {
+  todos: PropTypes.array,
+  toggleTodo: PropTypes.func,
+}
+
+TodoListItem.propTypes = {
+  index: PropTypes.number,
+  todo: PropTypes.object,
+  toggleTodo: PropTypes.func,
 }
 
 export default Todo;
